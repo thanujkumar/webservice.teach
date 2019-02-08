a6f3e3ddd50324d5d13cfc9439bddc3a9a38cdc6
Thanuj Kumar Sathyappa Chowdappa <thanuj.kumar.sathyappa.chowdappa@evry.com>
2019-02-08 13:51:26 +0530

    Working without @XmlRootElement

--- springws/src/main/java/org/tk/springws/endpoint/CountryServiceEndpoint.java
+++ springws/src/main/java/org/tk/springws/endpoint/CountryServiceEndpoint.java
@@ -3,6 +3,8 @@ package org.tk.springws.endpoint;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBIntrospector;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 
@@ -13,6 +15,7 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import org.tk.domain.countryprocessingaction.v1.CountryType;
 import org.tk.springws.service.CountryService;
+import org.tk.ws.countryprocessingaction.v1.ObjectFactory;
 
 @Endpoint // (Special sort of @Component)
 public class CountryServiceEndpoint {
@@ -21,15 +24,33 @@ public class CountryServiceEndpoint {
 
 	@Autowired
 	private CountryService countryService;
+	
+	//http://blog.bdoughan.com/2012/07/jaxb-and-root-elements.html
+	
+	//As the element is global no @XmlRootElement annotation is generated
 
+//	@PayloadRoot(localPart = "createCountryRequest", namespace = "urn:tk.org:ws:countryProcessingAction:v1")
+//    @ResponsePayload
+//	public CountryType createCountryRequest(@RequestPayload CountryType countryInfo) throws DatatypeConfigurationException {
+//        System.out.println(countryInfo);
+//        GregorianCalendar gcal = new GregorianCalendar();
+//        gcal.setTime(new Date());
+//        countryInfo.setCreatedTimestamp(DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal));
+//        countryInfo.setId(1L);
+// 		return countryInfo;
+//	}
+	
+	
 	@PayloadRoot(localPart = "createCountryRequest", namespace = "urn:tk.org:ws:countryProcessingAction:v1")
     @ResponsePayload
-	public CountryType createCountryRequest(@RequestPayload CountryType countryInfo) throws DatatypeConfigurationException {
+	public JAXBElement<CountryType> createCountryRequest(@RequestPayload JAXBElement<CountryType> countryInfoEle) throws DatatypeConfigurationException {
+		CountryType countryInfo = (CountryType) JAXBIntrospector.getValue(countryInfoEle);
         System.out.println(countryInfo);
         GregorianCalendar gcal = new GregorianCalendar();
         gcal.setTime(new Date());
         countryInfo.setCreatedTimestamp(DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal));
         countryInfo.setId(1L);
- 		return countryInfo;
+ 		return new ObjectFactory().createCreateCountyResponse(countryInfo);
 	}
+
 }
---
+++ springws/src/main/java/org/tk/springws/service/Test.java
@@ -0,0 +1,51 @@
+package org.tk.springws.service;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.JAXBIntrospector;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+
+import org.tk.domain.countryprocessingaction.v1.CountryType;
+import org.tk.ws.countryprocessingaction.v1.ObjectFactory;
+
+public class Test {
+
+	public static void main(String[] args) throws JAXBException, DatatypeConfigurationException {
+		JAXBContext context = JAXBContext.newInstance("org.tk.ws.countryprocessingaction.v1");
+		CountryType type = new CountryType();
+		type.setCreatedBy("TK");
+		type.setName("INDIA");
+
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(new Date());
+		type.setCreatedTimestamp(DatatypeFactory.newInstance().newXMLGregorianCalendar(gc));
+
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		// As CountryType has no @XmlRootElement
+		ObjectFactory factory = new ObjectFactory();
+		JAXBElement<CountryType> countryTypeElement = factory.createCreateCountryRequest(type);
+		marshaller.marshal(countryTypeElement, System.out);
+
+		try {
+			// As CountryType has no @XmlRootElement this would fail
+			marshaller.marshal(type, System.out); // will fail
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		ObjectFactory of = new ObjectFactory();
+		JAXBElement<CountryType> ct = of.createCreateCountryRequest(type);
+		marshaller.marshal(ct, System.out);
+	}
+}
