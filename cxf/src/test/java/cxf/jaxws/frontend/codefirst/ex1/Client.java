2d94f038314d75357dffc3976d961062bd2bbf86
Thanuj Kumar Sathyappa Chowdappa <thanuj.kumar.sathyappa.chowdappa@evry.com>
2019-04-16 12:43:46 +0530

    Added client as part of test

---
+++ cxf/src/test/java/cxf/jaxws/frontend/codefirst/ex1/Client.java
@@ -0,0 +1,20 @@
+package cxf.jaxws.frontend.codefirst.ex1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+	
+	public static void main(String[] args) throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"cxf/jaxws/frontend/codefirst/ex1/client-ex1.xml"});
+	    OrderProcess client = (OrderProcess) context.getBean("orderClient");
+		
+
+		Order order = new Order();
+		order.setCustomerId("123A");
+		order.setItemId("SomeItem");
+		order.setPrice(2.3);
+		order.setQuantity(10);
+	    System.out.println( client.processOrder(order) );
+	}
+
+}
